53cfb84c0a7401e37dd672a85cf3c25b7df5acbb
Martin Klefas-Stennett <cleverbum@hotmail.com>
2023-03-08 16:33:38 +0000

    wip: reading some parts of RDS, not others.

--- database_utils.py
+++ database_utils.py
@@ -1,2 +1,28 @@
+import yaml, sqlalchemy
+
+
+
 class DatabaseConnector:
+
+    def read_db_creds (self):
+        stream = open('db_creds.yaml', 'r') 
+        credentials = yaml.safe_load(stream)
+        return credentials
+
+    def init_db_engine(self):
+        dictCredentials = self.read_db_creds()
+        connectionString = "postgresql+psycopg2://" + dictCredentials["RDS_USER"] + ":" + dictCredentials["RDS_PASSWORD"]
+        connectionString += "@" + dictCredentials["RDS_HOST"] + ":" + str(dictCredentials["RDS_PORT"]) + "/" + str(dictCredentials["RDS_DATABASE"])
+        
     
+        engine = sqlalchemy.create_engine(connectionString)
+        return engine
+    
+    def list_db_tables (self, engine: sqlalchemy.engine.Engine):
+        with engine.connect() as connection:
+            result = connection.execute(sqlalchemy.text("SELECT * FROM pg_catalog.pg_tables WHERE schemaname != 'pg_catalog' AND     schemaname != 'information_schema';"))
+            table_names = list()
+            for row in result:
+                table_names.append(row[1])
+            
+            return table_names
